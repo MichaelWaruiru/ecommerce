ef284d9670b73ecade11df164d76f90c1c046a8d
Your Name <you@example.com>
2024-07-12 00:33:54 +0300

    Added PayPal payment method

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import mysql.connector
 import bcrypt
 from config import read_db_config
 from dotenv import load_dotenv
-
-load_dotenv()
+load_dotenv() # Load environment variables from .env file
 
 import os
 from datetime import timedelta, datetime, timezone
 from flask_mail import Mail, Message
-
 import secrets
+
+import paypalrestsdk
+
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = os.getenv("SECRET_KEY", default="DefaultSecretKey")
@@ -27,6 +28,13 @@ app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_DEFAULT_SENDER")
 mail = Mail(app)
 
 
+paypalrestsdk.configure({
+    "mode": "sandbox", # Sandbox for testing, live for production
+    "client_id": os.getenv("PAYPAL_CLIENT_ID"),
+    "client_secret": os.getenv("PAYPAL_CLIENT_SECRET")
+})
+
+
 def get_db_connection():
     try:
         db_config = read_db_config()
@@ -35,28 +43,29 @@ def get_db_connection():
     except Exception as e:
         print(f"Error connecting to the database: {e}")
         return None
-
-
+    
 @app.before_request
 def before_request():
     if "username" in session:
         if request.endpoint in ["login", "signup", "home"]:
-            session["last_activity"] = datetime.now(timezone.utc)  # Make time offset-aware
+            session["last_activity"] = datetime.now(timezone.utc) # Make time offset-aware
 
 
 # Checks user's last activity timestamp and logs them out
 @app.before_request
 def check_session_timeout():
     if "username" in session and "last_activity" in session:
-        last_activity = session["last_activity"].replace(tzinfo=timezone.utc)  # Make time offset-aware
+        last_activity = session["last_activity"].replace(tzinfo=timezone.utc) # Make time offset-aware
         excluded_endpoints = ["login", "signup", "home"]
-        
+
         is_refreshed = request.args.get("refresh") == "true"
-        
+
         if not is_refreshed and request.endpoint not in excluded_endpoints:
             if datetime.now(timezone.utc) - last_activity > app.permanent_session_lifetime:
                 session.pop("username", None)
                 return redirect(url_for("login"))
+            else:
+                session["last_activity"] = datetime.now(timezone.utc) # Update last activity
 
 
 # @app.route("/check_session", methods=["GET"])
@@ -72,37 +81,38 @@ def check_session_timeout():
 #         return jsonify("session_active")
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
         # print("Received POST request for signup")
         email = request.form.get("email")
         username = request.form.get("username")
         password = request.form.get("password")
-        
+
         if username_exists(username):
             flash("Username already exists. Please choose another username.", "error")
             return render_template("signup.html", error="Registration failed")
-        
+
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
         confirmation_token = secrets.token_urlsafe(30)
         token_expiration = datetime.now() + timedelta(hours=2)
-        
+        send_confirmation_email(email, confirmation_token)
+
         try:
             conn = get_db_connection()
             if conn is None:
                 print("Error connecting to the database.")
                 return
             cursor = conn.cursor()
-            
+
             query = "INSERT INTO users (email, username, password, confirmation_token, is_confirmed, token_expiration) VALUES (%s, %s, %s, %s, %s, %s)"
             cursor.execute(query, (email, username, hashed_password, confirmation_token, False, token_expiration))
             conn.commit()
-            
+
             print(f"Received: Email - {email}, Username - {username}, Password - {password}")
-            
-            send_confirmation_email(email, confirmation_token)
-            
+
+        
+
             return redirect(url_for("login"))
         except Exception as e:
             print(f"Error registering user: {e}")
@@ -113,7 +123,7 @@ def signup():
                 cursor.close()
             if conn:
                 conn.close()
-    
+
     return render_template("signup.html")
 
 
@@ -147,12 +157,10 @@ def confirm_email(token):
             username, token_expiration = result
             if datetime.utcnow() > token_expiration.replace(tzinfo=None):
                 flash("The confirmation link has expired. Please request a new one.", "error")
-                return render_template("confirm_email.html", success=False,
-                                       error_message="The confirmation link has expired")
-
+                return render_template("confirm_email.html", success=False, error_message="The confirmation link has expired")
+            
             # This marks the user as confirmed
-            cursor.execute("UPDATE users SET is_confirmed = TRUE, token_expiration = NULL WHERE username = %s",
-                           (username,))
+            cursor.execute("UPDATE users SET is_confirmed = TRUE, token_expiration = NULL WHERE username = %s", (username,))
             conn.commit()
             flash("Email confirmed successfully. You can log in.", "success")
             return redirect(url_for("login"))
@@ -162,8 +170,7 @@ def confirm_email(token):
     except Exception as e:
         print(f"Error confirming email: {e}")
         flash("Error confirming email. Please try again.", "error")
-        return render_template("confirm_email.html", success=False,
-                               error_message="Error confirming email. Please try again.")
+        return render_template("confirm_email.html", success=False, error_message="Error confirming email. Please try again.")
     finally:
         if cursor:
             cursor.close()
@@ -177,11 +184,11 @@ def username_exists(username):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
-        
+
         query = "SELECT username FROM users WHERE username = %s"
-        cursor.execute(query, (username,))
+        cursor.execute(query,(username,))
         result = cursor.fetchone()
-        
+
         return result is not None
     
     except Exception as e:
@@ -198,38 +205,39 @@ def user_authenticated(username, password):
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
-        
+
         query = "SELECT password, is_confirmed FROM users WHERE username = %s"
         cursor.execute(query, (username,))
         result = cursor.fetchone()
-        
+
         # if result:
-        hashed_password, is_confirmed = result
-        if bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8")) and is_confirmed:
-            return True
-    
+        #     hashed_password, is_confirmed = result
+        #     if bcrypt.checkpw(password.encode("utf-8"), hashed_password.encode("utf-8")) and is_confirmed:
+        #         return True
+
         if result:
             return True
-        
+           
         conn.close()
     except Exception as e:
         print(f"Error authenticating user: {e}")
     return False
 
 
-@app.route("/login", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        
+
         if user_authenticated(username, password):
             session["username"] = username
+            session["last_activity"] = datetime.now(timezone.utc) # Set last activity
             return redirect(url_for("home", username=username))
         else:
             flash("Session expired.Please log in again.", "info")
             return render_template("login.html", error="Authentication failed")
-    
+        
     return render_template("login.html")
 
 
@@ -244,8 +252,8 @@ def home(username):
     if "username" in session and session["username"] == username:
         return render_template("app.html", username=username)
     else:
-        return redirect(url_for("login"))
-
+         return redirect(url_for("login"))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
